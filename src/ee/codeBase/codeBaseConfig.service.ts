7c244604b9db151570a4d9f06bb8a720e2d1f3da
Jairo Litman <130161309+jairo-litman@users.noreply.github.com>
2025-10-17 15:57:21 -0300

    fix empty array

--- src/ee/codeBase/codeBaseConfig.service.ts
+++ src/ee/codeBase/codeBaseConfig.service.ts
@@ -128,7 +128,7 @@ export default class CodeBaseConfigService implements ICodeBaseConfigService {
                 repository,
                 parameters?.configValue,
                 defaultBranch,
-                preliminaryFiles,
+                preliminaryFiles || [],
             );
 
             const kodyRules = this.kodyRulesValidationService.filterKodyRules(
@@ -241,8 +241,9 @@ export default class CodeBaseConfigService implements ICodeBaseConfigService {
 
         const repoDelta = repoConfig?.configs;
 
-        const affectedPaths =
-            this.extractUniqueDirectoryPaths(preliminaryFiles);
+        const affectedPaths = this.extractUniqueDirectoryPaths(
+            preliminaryFiles || [],
+        );
 
         const directoryConfig = this.resolveConfigByDirectories(
             organizationAndTeamData,
@@ -824,7 +825,7 @@ export default class CodeBaseConfigService implements ICodeBaseConfigService {
     ): string[] {
         const paths = new Set<string>();
 
-        files.forEach((file) => {
+        (files || []).forEach((file) => {
             const lastSlashIndex = file.filename.lastIndexOf('/');
 
             if (lastSlashIndex > 0) {
