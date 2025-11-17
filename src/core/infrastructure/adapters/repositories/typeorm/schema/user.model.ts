33acee8b64b9a7a859ecdd29d8e324613e225287
Jairo Litman <130161309+jairo-litman@users.noreply.github.com>
2025-11-17 15:11:12 -0300

    new baseline migration

--- src/core/infrastructure/adapters/repositories/typeorm/schema/user.model.ts
+++ src/core/infrastructure/adapters/repositories/typeorm/schema/user.model.ts
@@ -1,21 +1,24 @@
+import { STATUS } from '@/config/types/database/status.type';
+import { Role } from '@/core/domain/permissions/enums/permissions.enum';
 import { CoreModel } from '@/shared/infrastructure/repositories/model/typeOrm';
 import {
     Column,
     Entity,
+    Index,
     JoinColumn,
     ManyToOne,
     OneToMany,
     OneToOne,
 } from 'typeorm';
-import { OrganizationModel } from './organization.model';
-import { ProfileModel } from './profile.model';
 import { AuthModel } from './auth.model';
-import { TeamMemberModel } from './teamMember.model';
-import { STATUS } from '@/config/types/database/status.type';
-import { Role } from '@/core/domain/permissions/enums/permissions.enum';
+import { OrganizationModel } from './organization.model';
 import { PermissionsModel } from './permissions.model';
+import { ProfileModel } from './profile.model';
+import { TeamMemberModel } from './teamMember.model';
 
 @Entity('users')
+@Index('IDX_users_email', ['email'], { concurrent: true })
+@Index('IDX_users_org_status', ['organization', 'status'], { concurrent: true })
 export class UserModel extends CoreModel {
     @Column({ unique: true, nullable: false })
     email: string;
