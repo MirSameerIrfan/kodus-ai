2a71fcb894cfac037a65873582d7bce4febc136b
Jairo Litman <130161309+jairo-litman@users.noreply.github.com>
2025-12-04 16:05:45 -0300

    fix env

--- scripts/fetch-env-qa.sh
+++ scripts/fetch-env-qa.sh
@@ -141,6 +141,9 @@ KEYS=(
     "/qa/kodus-orchestrator/API_KODUS_MCP_SERVER_URL"
 
     "/qa/kodus-orchestrator/API_OPENROUTER_KEY"
+
+    "/qa/kodus-orchestrator/API_URL"
+    "/qa/kodus-orchestrator/API_FRONTEND_URL"
 )
 
 # Lista de todas as chaves que você precisa
